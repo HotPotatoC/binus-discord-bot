9251c4b4f433558aa735ae2b29da8983d46e6c78
HotPotatoC <juandotulung@gmail.com>
2021-10-08 20:29:56 +0700

    refactor(http-client headers): updated `User-Agent`

--- src/infrastructure/http-client.ts
+++ src/infrastructure/http-client.ts
@@ -12,7 +12,7 @@ export const request = axios.create({
     rOld: '1059b47e-695d-4476-9950-bb8de0454ca0',
     roleId: '4bcb81bd-46a8-4a09-a923-5e812cb7007b',
     'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0',
     'Content-Type': 'application/json',
   },
 })
