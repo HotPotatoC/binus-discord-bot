4d37522b176887ed47b7e2965aceaddf3b1f3030
HotPotatoC <juandotulung@gmail.com>
2021-10-08 17:51:39 +0700

    feat: added axios instance

---
+++ src/infrastructure/request.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import { auth, binus } from '../config'
+
+export const instance = axios.create({
+  headers: {
+    Origin: binus.binusMayaURL,
+    Referer: binus.binusMayaURL,
+    roleName: 'Student',
+    Authorization: `Bearer ${auth.bearerToken}`,
+    academicCareer: 'RS1',
+    institution: 'BNS01',
+    rOld: '1059b47e-695d-4476-9950-bb8de0454ca0',
+    roleId: '4bcb81bd-46a8-4a09-a923-5e812cb7007b',
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+    'Content-Type': 'application/json',
+  },
+})
+
+export default instance
--- src/services/schedule.ts
+++ src/services/schedule.ts
@@ -1,9 +1,8 @@
-import axios from 'axios'
 import { ObjectId } from 'bson'
 import { nanoid } from 'nanoid'
 
-import { auth, binus } from '../config'
 import { routeScheduleDate } from '../types'
+import request from '../infrastructure/request'
 import type { ServiceContext, ScheduleResponse } from '../types'
 import type { ScheduleDomainContext } from '../domain/schedule-domain'
 
@@ -43,21 +42,7 @@ export function createScheduleService({
         ],
       }
 
-      const { data } = await axios.post(routeScheduleDate(date), payload, {
-        headers: {
-          Origin: binus.binusMayaURL,
-          Referer: binus.binusMayaURL,
-          roleName: 'Student',
-          Authorization: `Bearer ${auth.bearerToken}`,
-          academicCareer: 'RS1',
-          institution: 'BNS01',
-          rOld: '1059b47e-695d-4476-9950-bb8de0454ca0',
-          roleId: '4bcb81bd-46a8-4a09-a923-5e812cb7007b',
-          'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-          'Content-Type': 'application/json',
-        },
-      })
+      const { data } = await request.post(routeScheduleDate(date), payload)
 
       const { dateStart } = data as unknown as ScheduleResponse
       const schedule = (data as unknown as ScheduleResponse).Schedule
