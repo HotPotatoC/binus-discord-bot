516690500c31c6cc0a7a4d3a6bec8bbec263f440
HotPotatoC <juandotulung@gmail.com>
2021-10-08 15:45:51 +0700

    feat: reimplement ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,11 +1,21 @@
+import type { Message } from 'discord.js'
 import type { Command, CommandContext } from './../types'
 
 export async function pingExecute({ interaction }: CommandContext) {
-  await interaction.reply('Pong!')
+  const sent = (await interaction.reply({
+    content: 'Pinging...',
+    fetchReply: true,
+  })) as Message
+
+  await interaction.editReply(
+    `Roundtrip latency: ${
+      sent.createdTimestamp - interaction.createdTimestamp
+    }ms`
+  )
 }
 
 export default {
   name: 'ping',
-  description: 'Replies with pong!',
+  description: 'Pings the bot (Not to be confused with your ping)',
   execute: pingExecute,
 } as Command
